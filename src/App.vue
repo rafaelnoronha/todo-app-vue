b66a2ef64748035a85436411eaec12e97b3b7610
Rafael Noronha <rafaelsnoronha@hotmail.com>
2023-05-16 17:11:36 -0300

    Primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+    import ContentCenter from '@/components/ContentCenter.vue';
+</script>
+
+<template>
+    <div class="container d-flex justify-content-center">
+        <ContentCenter>
+            <h1 class="mt-5 fs-5rem"> Things To Do</h1>
+        </ContentCenter>
+    </div>
+</template>
+
+<style scoped></style>
