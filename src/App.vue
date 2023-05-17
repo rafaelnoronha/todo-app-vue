37896c6b226bdb99e0cd0c3e96bc2ed09a97c773
Rafael Noronha <rafaelsnoronha@hotmail.com>
2023-05-17 19:35:43 -0300

    Make base layout

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,51 @@
 <script setup>
-    import ContentCenter from '@/components/ContentCenter.vue';
+    import { onMounted, ref } from 'vue'
+
+    import ContentCenter from '@/components/ContentCenter.vue'
+    import Task from '@/components/Task.vue'
+
+    const task = ref( '' )
+    const taskList = ref( [] )
+
+    const includeTask = () => {
+        const newTaskList = taskList.value
+
+        newTaskList.push( {
+            description: task,
+            status: 0
+        } )
+
+        taskList.value = newTaskList
+        task.value = ''
+    }
+
+    onMounted( () => {
+        console.log( taskList.value )
+    } )
+
 </script>
 
 <template>
-    <div class="container d-flex justify-content-center">
+    <div class="container">
         <ContentCenter>
-            <h1 class="mt-5 fs-5rem"> Things To Do</h1>
+            <h1 class="mt-5 fs-5rem text-center"> Task To Do</h1>
+
+            <div class="row justify-content-center mt-5">
+                <div class="col-md-6 d-flex justify-content-center">
+                    <div class="col-md-8">
+                        <input class="w-100 ff-shadows border border-black px-3 py-2" type="text" placeholder="Things" v-model="task" />
+                    </div>
+
+                    <div class="col-md-4">
+                        <button class="btn btn-dark ff-shadows rounded-0 px-5 h-100" type="button" @click="includeTask"><i class="bi bi-plus ff-shadows" /> Include</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row mt-5">
+                <Task v-for="(task, index) in taskList" :key="index">{{ task.description }}</Task>
+            </div>
+
         </ContentCenter>
     </div>
 </template>
-
-<style scoped></style>
---
+++ src/components/Task.vue
@@ -0,0 +1,40 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="col-md-3">
+        <div class="card text-bg-light mb-3 ff-shadows rounded-0">
+            <div class="card-header d-flex justify-content-between p-0">
+                <div class="header-button position-relative ms-1">
+                    <i class="bi bi-pin-angle-fill position-absolute top-50 start-50 translate-middle" />
+                </div>
+                <div class="header-button position-relative">
+                    <button class="btn btn-light w-100 h-100 rounded-0">
+                        <i class="bi bi-hand-thumbs-down-fill position-absolute top-50 start-50 translate-middle" />
+                    </button>
+                </div>
+            </div>
+            <div class="card-body">
+                <p class="card-text">
+                    <slot />
+                </p>
+            </div>
+            <div class="card-footer p-0">
+                <button class="btn btn-dark w-100 rounded-0" type="button"><i class="bi bi-trash-fill" /> Exclude</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    ::selection {
+        background: #ffffff;
+        color: #212529;
+    }
+
+    .header-button {
+        height: 30px!important;
+        width: 30px!important;
+    }
+</style>
